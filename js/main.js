8a9ca1a42394d43cac15f68b352ad238184cc5e1
maxclayton <maxclayton07@gmail.com>
2020-04-17 17:59:36 -0600

    done done

---
+++ js/main.js
@@ -0,0 +1,18 @@
+function duplicateMenu() {
+    let topList = document.querySelectorAll('ul#primaryNavigation li a')
+    let newList = document.createElement('ul')
+    let bottomNav = document.getElementById('smallNavArea')
+
+    topList.forEach(menuItem => {
+        let newLI = document.createElement('li')
+        let newLink = document.createElement('a')
+        newLink.setAttribute('href', menuItem.getAttribute('href'))
+        newLink.textContent = menuItem.textContent
+        newLI.appendChild(newLink)
+        newList.appendChild(newLI)
+    });
+
+    bottomNav.appendChild(newList)
+}
+
+duplicateMenu()
